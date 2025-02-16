7717606cc422bc4684322b6724d8b13b046f7acb
Yatin Sharma <22150530+yatinsharma7@users.noreply.github.com>
2025-02-16 22:55:06 +0530

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    if(a<0 || b<0) return "ERROR";
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';    
+    if (a<b) {
+        for (let index = a; index <= b; index++) {
+            sum = sum + index;
+            
+        }
+        return sum;
+    } else if(a>b) {
+        for (let index = b; index <=a; index++) {
+            sum += index;
+            
+        }
+        return sum;
+    } 
+
+
+
 
 };
 
